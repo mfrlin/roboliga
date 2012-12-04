313d314ec46f9d35e75350b8cf383ed22219285a
Kristjan Ribolica <riba1122@gmail.com>
2012-12-04 15:45:54 +0100

    minor changes

--- src/com/lazerspewpew/StartPointNoComm.java
+++ src/com/lazerspewpew/StartPointNoComm.java
@@ -12,14 +12,14 @@ public class StartPointNoComm {
 	 */
 	public static void main(String[] args) {
 		int robotPower = 95;
-		/*LineRobot crta = new LineRobot(MotorPort.A, MotorPort.B, SensorPort.S3, SensorPort.S2, robotPower);
+		LineRobot crta = new LineRobot(MotorPort.A, MotorPort.B, SensorPort.S3, SensorPort.S2, robotPower);
 		Button.waitForAnyPress();
 		crta.followLine();
-		*/
-		WallRobot wall = new WallRobot(MotorPort.A, MotorPort.B, SensorPort.S1, SensorPort.S4, robotPower);
-		LCD.clear();
-		LCD.drawString("wating for button press", 0, 0);
-		Button.waitForAnyPress();
-		wall.followWall();
+		
+//		WallRobot wall = new WallRobot(MotorPort.A, MotorPort.B, SensorPort.S1, SensorPort.S4, robotPower);
+//		LCD.clear();
+//		LCD.drawString("wating for button press", 0, 0);
+//		Button.waitForAnyPress();
+//		wall.followWall();
 	}
 }
